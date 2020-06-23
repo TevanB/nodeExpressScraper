c14ed8a6b62f751e9e4c1a3e09edd153ac4605ac
bhatiate <tevan.bhatia@mail.utoronto.ca>
2020-06-23 00:49:37 -0400

    Setting up database info heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Str;
 
+$user = parse_url(getenv("DATABASE_URL"));
+$hsot = $url["host"]??null;
+$username = $url["user"]??null;
+$password = $url["pass"]??null;
+$database = substr($url["path"], 1)??null;
 return [
 
     /*
@@ -15,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +82,16 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
-
+        'pgsql_production' => [
+          'driver' => 'pgsql',
+          'host' => $host,
+          'database' => $database,
+          'username' => $username,
+          'password' => $password,
+          'charset' => 'utf8',
+          'prefix' => '',
+          'schema' => 'public'
+        ]
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
