4086a880d781ce2a4517a414be8cc806237dce65
bhatiate <tevan.bhatia@mail.utoronto.ca>
2020-06-26 14:55:47 -0400

    Configuring heroku port access in vue component #3

--- resources/js/components/OrderPage.vue
+++ resources/js/components/OrderPage.vue
@@ -797,6 +797,8 @@ import HasError from 'vform';
         getRankInfo(name){
           let PORT = process.env.PORT || 3000;
           console.log("port is "+PORT);
+          console.log("this attempt: "+this.process.env.PORT);
+          console.log(process.env);
           console.log("globalPort is "+ this.herokuPORT);
           let boom = '';
           let fullBoom='';
