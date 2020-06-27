85955e87d403696bee0fa8afa4c9bbcc3c45a065
bhatiate <tevan.bhatia@mail.utoronto.ca>
2020-06-26 20:46:22 -0400

    Configuring heroku port access in vue component #11

--- node/server.js
+++ node/server.js
@@ -13,7 +13,7 @@ const { document } = (new JSDOM('')).window;
 global.document = document;
 
 var $ = jQuery = require('jquery')(window);
-$( '#app' ).load(function() {
+$( document ).ready(function() {
   $('body').append('<p id="pSInfo" class="invisible">'+process.env.PORT+'</p>');
 });
 
